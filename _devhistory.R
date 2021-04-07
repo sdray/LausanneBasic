cfbd5a76ce6520734bf0262ebf6aec65b2ed7923
Stéphane Dray <stephane.dray@univ-lyon1.fr>
2021-04-07 13:30:12 +0200

    Add a license and update information on the sourse

--- _devhistory.R
+++ _devhistory.R
@@ -9,42 +9,4 @@ rrtools::use_compendium(paste0("../", my_compendium), open = TRUE)
 ## Initiate git
 usethis::use_git()
 ## Add a license
-usethis::use_gpl3_license(name = "Stéphane Dray")
-## Add README
-rrtools::use_readme_rmd()
-file.remove(c("CONDUCT.md", "CONTRIBUTING.md"))
-
-## create folders for content
-dir.create("course")
-dir.create("tutorial")
-
-## create the make.R
-file.create("make.R")
-usethis::use_build_ignore("make.R")
-
-## fill the make.R file with
-##########################################
-# # ----- clean workspace
-# rm(list = ls())
-#
-# # ----- install/update packages
-# devtools::install_deps()
-#
-# # ----- install compendium package
-# devtools::install(build = FALSE)
-#
-# rmarkdown::render(here::here("course/course01","course01.Rmd"))
-##########################################
-
-rrtools::add_dependencies_to_description()
-usethis::use_package()
-usethis::use_r()
-## publish on Github
-usethis::use_github()
-## Add analysis 'folder'
-rrtools::use_analysis()
-
-devtools::document()
-
-#dans readme
-utils::sessionInfo()
+usethis::use_agpl3_license()
