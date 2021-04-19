32882136b1d271b99bc550c415c1edd6881fbf4c
Stéphane Dray <stephane.dray@univ-lyon1.fr>
2021-04-19 09:50:18 +0200

    Update compilation of all files

--- make.R
+++ make.R
@@ -9,7 +9,13 @@ devtools::install(build = FALSE)
 
 ##build html
 rmarkdown::render(here::here("course/session1","session1.Rmd"))
+rmarkdown::render(here::here("course/session2","session2.Rmd"))
 rmarkdown::render(here::here("practical/session1","session1.Rmd"))
+rmarkdown::render(here::here("practical/session2","session2.Rmd"))
+rmarkdown::render(here::here("practical/session2","session2.Rmd"), params = list(correction = TRUE), output_file = "session2-corrected")
 ## build pdf
-pagedown::chrome_print(here::here("course/session1","session1.Rmd"))
-pagedown::chrome_print(here::here("practical/session1","session1.Rmd"))
+pagedown::chrome_print(here::here("course/session1","session1.html"))
+pagedown::chrome_print(here::here("course/session2","session2.html"))
+pagedown::chrome_print(here::here("practical/session1","session1.html"))
+pagedown::chrome_print(here::here("practical/session2","session2.html"))
+pagedown::chrome_print(here::here("practical/session2","session2-corrected.html"))
